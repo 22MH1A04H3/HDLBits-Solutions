4da1eba1b91212e45bcb1f7f79884e43e6434411
KODA DASU <119719005+22MH1A04H3@users.noreply.github.com>
2024-12-28 22:48:35 +0530

    popcount.v

---
+++ popcount.v
@@ -0,0 +1,18 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+    always@(*)
+        begin
+            out=8'b00000000;
+            for(int i=0;i<255;i++)
+                begin
+                    if(in[i]==1'b1)
+                     
+                    out=out+1;
+                      
+                  else 
+                      out=out;
+                    
+                end
+        end
+endmodule

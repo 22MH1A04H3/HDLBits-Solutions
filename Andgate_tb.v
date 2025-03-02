577ff49a50b0386a84a19251a23ee0fe10393634
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-03-02 18:04:18 +0530

    Create Andgate_tb.v

---
+++ Andgate_tb.v
@@ -0,0 +1,18 @@
+module top_module();
+    reg [1:0]in;
+    wire out;
+    andgate uut(.in(in),.out(out));
+    initial 
+        begin
+            in[1]=1'b0;in[0]=1'b0;
+            #10;
+            in[1]=1'b0;in[0]=1'b1;
+            #10;
+            in[1]=1'b1;in[0]=1'b0;
+            #10;
+            in[1]=1'b1;in[0]=1'b1;
+            #10;
+            
+            
+        end
+endmodule
